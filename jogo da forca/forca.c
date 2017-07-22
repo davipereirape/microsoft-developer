83563814ef72cb918a3a93b76b22bf80cb1125b4
Davi Pereira <pereiradavipe@gmail.com>
2017-07-22 16:27:24 -0300

    logic of game

--- jogo da forca/forca.c
+++ jogo da forca/forca.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main () 
 {
@@ -8,13 +9,35 @@ int main ()
     sprintf(palavrasecreta, "MELANCIA"); 
     palavrasecreta[9] = '\0'; // Indica para o printf até onde ele deve verificar o array de char
 
-    printf("%s\n", palavrasecreta);
 
     // JOGO...
-    int acertou = 0;
-    int enforcado = 0;
+    char chutes[26]; // quantidade do alfabeto
+    int tentativas = 0, acertou = 0, enforcado = 0;
     do 
     {
+        // Verifica se achou a letra na palavra, printando a letra ou o traço de espaço vazio.
+        for (int i = 0; i < strlen(palavrasecreta); i++)
+        {
+            int achou = 0;
+            for (int j = 0; j < tentativas; j++)
+            {
+                if (chutes[j] == palavrasecreta[i])
+                {
+                    achou = 1;
+                    break;
+                }
+            }
+            if (achou) 
+                printf("%c ", palavrasecreta[i]);
+            else
+                printf("__ ");  
+        }
+        printf("\n");
+
+        char chute; 
+        scanf(" %c", &chute); // O Espaço ignora o ENTER
+        chutes[tentativas] = chute;
+        tentativas++;
 
     } while (!acertou && !enforcado);
 }
